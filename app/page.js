98b9a3e14168840283a0b8d847459f597faa3d87
RobFrontend <robert.grabowski97@gmail.com>
2025-02-01 15:54:15 +0100

    rwd

--- app/page.js
+++ app/page.js
@@ -20,19 +20,23 @@ export default async function Home() {
     );
   return (
     <>
-      <section className="min-h-screen  text-slate-100 grid content-center">
+      <section className="min-h-screen  text-slate-100 grid content-center px-8 max-md:px2">
         <nav className="absolute top-4 right-4">
           <Link
             href="/cms"
-            className="text-xl bg-slate-100 text-slate-800 px-4 py-2 hover:opacity-75 transition-all duration-300"
+            className="text-xl bg-slate-100 text-slate-800 px-4 py-2 hover:opacity-75 transition-all duration-300 max-xl:text-lg max-md:text-base"
           >
             Panel Edycji
           </Link>
         </nav>
         <div className="text-center">
-          <h1 className="text-7xl uppercase">{data.heroH1}</h1>
-          <h2 className="text-6xl">{data.heroH2}</h2>
-          <div className="flex gap-8 justify-center text-2xl py-8 ">
+          <h1 className="text-7xl uppercase max-xl:text-5xl max-md:text-3xl mb-3">
+            {data.heroH1}
+          </h1>
+          <h2 className="text-6xl max-xl:text-3xl max-md:text-2xl">
+            {data.heroH2}
+          </h2>
+          <div className="flex gap-8 justify-center text-2xl py-8 max-xl:text-lg max-[500px]:flex-col">
             <Link
               href={data.herobtn1url}
               className="px-6 py-4 border-slate-50 border-2 hover:opacity-75 transition-all duration-300"
@@ -56,13 +60,22 @@ export default async function Home() {
           className="-z-10"
         />
       </section>
-      <section className="text-slate-800">
+      <section className="text-slate-800 px-8 max-md:px-2">
         <div className="max-w-[1200px] mx-auto my-0">
           <h2 className="text-2xl">{data.abouth2}</h2>
-          <div className="flex gap-8 p-8">
-            <Image src={data.aboutkv} width={300} height={1000} alt="aboutkv" />
+          <div className="flex gap-8 p-8 max-md:flex-col max-md:px-2">
+            <Image
+              src={data.aboutkv}
+              width={300}
+              height={1000}
+              alt="aboutkv"
+              className="h-fit max-md:self-center"
+            />
+
             <div className="grid content-center">
-              <p className="text-lg text-center">{data.aboutdesc}</p>
+              <p className="text-lg text-center max-xl:text-base">
+                {data.aboutdesc}
+              </p>
             </div>
           </div>
         </div>
